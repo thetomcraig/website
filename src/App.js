09cc0a6978612f60573b6b132556ded4477dd83d
Tom Craig <thetomcraig@icloud.com>
2021-08-24 15:56:04 -0700

    Setup rows and spacing, image and description

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
+
+
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import ascii_pic from './ascii_pic.png';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-          Testing Change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Container className="p-3" xs="10" md="6">
+
+    <Row className="justify-content-md-center">
+      <h4
+        className="header"
+        style={{textAlign: "center"}}
+      >
+        TOMCRAIG.ORG
+      </h4>
+      <img 
+        src={ascii_pic}
+        style={{maxWidth: "24rem"}}
+      >
+      </img>
+    </Row>
+
+    <Row >
+      <p>
+        __ADD SPACE HERE__
+
+        I'm a software engineer living and working in the San Francisco Bay area.  Currently I'm working at Pandora Media, building front end tools for developers.  I've worked on advertising at Pandora and done research at NASA's Jet Propulsion Laboratory for the Juno Mission.  I make a podcast about culture called Total Immersion and I've recently setup an AI botnet.  I'm passionate about technology and I'm always learning something new.
+
+I'm currently learning how to solve a Rubik's Cube, and the lyrics to this song.
+      </p>
+    </Row>
+  </Container>
+);
 
 export default App;
+
