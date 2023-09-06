3febde742fd30bd577cf2d8b388a89e04ffefcde
Tom Craig <thetomcraig@icloud.com>
2023-09-05 18:15:40 -0700

    converting to typescript

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css"
+import { BrowserRouter } from "react-router-dom"
+import { Helmet, HelmetProvider } from "react-helmet-async"
+import HeaderComponent from "./components/header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <HelmetProvider>
+        <div>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>TomCraig.org</title>
+            <link rel="canonical" href="http://tomcraig.org" />
+          </Helmet>
+          <div className="container">
+            <HeaderComponent />
+          </div>
+        </div>
+      </HelmetProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import "./header.css";
+
+function HeaderComponent() {
+  return (
+    <div>
+      <div className="application-header">
+        <Typography className="header-text" variant="h4">
+          TomCraig.org
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderComponent;
