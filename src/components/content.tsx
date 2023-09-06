f4243741ba7ae51d7eca924c4c883d8a24b64e45
Tom Craig <thetomcraig@icloud.com>
2023-09-06 14:00:27 -0700

    rework the content to use Typography and Link

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,74 +1,64 @@
-import { Grid, Typography, Link } from "@mui/material"
-import { createTheme, Theme, useTheme, ThemeProvider } from "@mui/material/styles"
+import { Typography, Link } from "@mui/material"
 import "./content.css"
 
 function ContentComponent() {
-  const theme = createTheme({
-    breakpoints: {
-      values: {
-        xs: 0,
-        sm: 600,
-        md: 960,
-        lg: 1280,
-        xl: 1920,
-      },
-    },
-    // margin: {
-    //   xs: "20px",
-    //   sm: "30px",
-    //   md: "40px",
-    //   lg: "50px",
-    //   xl: "60px",
-    // },
-  })
-
-  const styles = (theme: Theme) => ({
-    root: {
-      [theme.breakpoints.up('md')]: {
-        margin: "10px",
-      },
-    },
-  })
-
-
   return (
-    // <ThemeProvider styles={styles}>
+    <div className="content-container">
       <div className="bio-container">
         <Typography className="bio-text" variant="h6">
           My name is Tom Craig, and I build things.
         </Typography>
         <Typography className="bio-text" variant="h6">
-          I do{" "}
+          - I do{" "}
           <Link href="https://www.upwork.com/freelancers/~017287fbe859ac7cbc?viewMode=1">
-            {" "}
             freelance web development.
           </Link>
         </Typography>
         <Typography className="bio-text" variant="h6">
-          I build and operate 3D printers.
+          - I build and operate 3D printers.
         </Typography>
         <Typography className="bio-text" variant="h6">
-          I have done front and back end software engineering for Pandora Media,
-          Welkin Health Ceres Imaging, and Coread Health, and I have been a
-          research fellow at NASA's Jet Propulsion Laboratory for the Juno
-          Mission.
+          - I have held front and back end software engineering roles at:
         </Typography>
         <Typography className="bio-text" variant="h6">
-          I make software in my free time, too.{" "}
+          <span>&emsp;&emsp;&emsp;&emsp;</span>
+          <Link href="https://www.pandora.com/">- Pandora Media</Link>
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          <span>&emsp;&emsp;&emsp;&emsp;</span>
+          <Link href="https://welkinhealth.com/">- Welkin Health</Link>
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          <span>&emsp;&emsp;&emsp;&emsp;</span>
+          <Link href="https://www.ceresimaging.net/">- Ceres Imaging</Link>
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          <span>&emsp;&emsp;&emsp;&emsp;</span>
+          <Link href="https://www.coread.io/">- Coread AI</Link>
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          - I worked as a Research Fellow at:{" "}
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          <span>&emsp;&emsp;&emsp;&emsp;</span>
+          <Link href="https://www.jpl.nasa.gov/">
+            NASA's Jet Propulsion Laboratory
+          </Link>{" "}
+          for the
+          <Link href="https://www.jpl.nasa.gov/missions/juno">
+            {" "}
+            Juno Mission.
+          </Link>
+        </Typography>
+        <Typography className="bio-text" variant="h6">
+          - I make software in my free time, too.{" "}
           <Link href="https://github.com/thetomcraig/chit">
             Here's one project.
           </Link>
         </Typography>
       </div>
-    // </ThemeProvider>
+    </div>
   )
 }
-// (You can see my profile{" "}
-// <a href="https://www.upwork.com/freelancers/~017287fbe859ac7cbc?viewMode=1">
-//   here!
-// </a>
-// )
-// I make software in my free time, too. (
-// <a href="https://github.com/thetomcraig/chit">Here's one project</a>)
 
 export default ContentComponent
